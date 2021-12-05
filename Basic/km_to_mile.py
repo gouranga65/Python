565b03edf5be3a0738ce48753fde46d6d4c45ceb
gouranga65 <gm49302@gmail.com>
2021-12-05 15:28:43 +0530

    added km to mile code

---
+++ Basic/km_to_mile.py
@@ -0,0 +1,7 @@
+# kilo meter to miles
+
+km = input("Enter kilo meter :: ")   # taking input from user
+mile = float(km) * 0.621371   # convert km string to int
+
+# round up upto 2 decimal places
+print(f"Your {km}km is eual to {round(mile,2)} miles")
