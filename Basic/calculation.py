f954eddb0e0bdfce7b8faaac61b8270ae18bbc20
gouranga65 <gm49302@gmail.com>
2021-12-05 15:50:59 +0530

    create a calculator program. - create addition method. - create subtraction method. - create multiplication method. - create division method.

---
+++ Basic/calculation.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter a number :: "))  # taking first input from user
+num2 = int(input("Enter a number :: "))  # taking second input from user
+
+# addition
+addition = num1+num2
+print("addition of two number is ::", addition)
+
+# substraction
+subsctration = num1-num2
+print("substraction of two number is ::", subsctration)
+
+# multiplication
+multiplication = num1*num2
+print("multiplication of two number is ::", multiplication)
+
+# division
+division = num1/num2    # for Int division we can use //
+print("division of two number is ::", division)
