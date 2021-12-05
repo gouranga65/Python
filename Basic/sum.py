1172760fa07d16339e56d05bb5d78d35c40a74b1
gouranga65 <gm49302@gmail.com>
2021-12-05 12:32:19 +0530

    add sum of two numbers

---
+++ Basic/sum.py
@@ -0,0 +1,15 @@
+# Add two Number and print the Output
+
+# Taking input of num 1
+print("Enter a number :: ")
+num1 = int(input())
+
+# Taking input of num 2
+print("enter a number::")
+num2 = int(input())
+
+# Add num1 and num2
+ans = num1+num2
+
+# print the answer
+print(ans)
